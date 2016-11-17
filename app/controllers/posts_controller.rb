848647546df3f4c545cc77ce7d5aef494a9e8afe
Mario Goodwin <mr.goodwin@outlook.com>
2016-11-17 06:15:45 -0500

    bootstrap form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,8 +10,7 @@ class PostsController < ApplicationController
 	end
 
 	def create
-		@post = Post.new(post_params)
-		@post.user_id = current_user.id
+		@post = current_user.posts.build(post_params)
 			if @post.save
 				flash.now[:success] = 'Post created'
 				redirect_to root_path
